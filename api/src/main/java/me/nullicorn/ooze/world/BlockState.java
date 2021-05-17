51722edff3744afd1005282c26c626875870c413
Nullicorn <bennullicorn@gmail.com>
2021-05-16 22:57:09 -0400

    Override equals() and hashCode() in BlockState

--- api/src/main/java/me/nullicorn/ooze/world/BlockState.java
+++ api/src/main/java/me/nullicorn/ooze/world/BlockState.java
@@ -1,5 +1,6 @@
 package me.nullicorn.ooze.world;
 
+import java.util.Objects;
 import lombok.Getter;
 import me.nullicorn.nedit.type.NBTCompound;
 import me.nullicorn.ooze.InvalidResourceLocationException;
@@ -88,5 +89,23 @@ public class BlockState {
   @Override
   public String toString() {
     return toNBT().toString();
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    BlockState that = (BlockState) o;
+    return name.equals(that.name) &&
+           Objects.equals(properties, that.properties);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name, properties);
   }
 }
