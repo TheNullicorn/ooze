dfe0bd50ce66007f51b47b97c2e5148e100171d6
Nullicorn <bennullicorn@gmail.com>
2021-06-07 21:01:27 -0400

    Micro-optimizations for ResourceLocation

--- common/src/main/java/me/nullicorn/ooze/ResourceLocation.java
+++ common/src/main/java/me/nullicorn/ooze/ResourceLocation.java
@@ -14,13 +14,8 @@ public class ResourceLocation {
   // Used when no namespace is provided.
   private static final String DEFAULT_NAMESPACE = "minecraft";
 
-  // Character used to separate namespace from path.
-  // Stored as a string to avoid char conversion in String#split().
-  private static final String PATH_INDICATOR = ":";
-
-  // Used to validate parsed resource locations.
-  private static final Pattern namespaceFormat = Pattern.compile("^[a-z0-9._\\-]+$");
-  private static final Pattern pathFormat      = Pattern.compile("^[a-z0-9/._\\-]+$");
+  // Sequence used to separate namespace from path.
+  private static final Pattern PATH_INDICATOR = Pattern.compile(":");
 
   /**
    * Constructs a resource location by parsing the provided <code>value</code>.
@@ -37,7 +32,7 @@ public class ResourceLocation {
     String namespace;
     String path;
 
-    String[] segments = value.split(PATH_INDICATOR, 3);
+    String[] segments = PATH_INDICATOR.split(value, 3);
     switch (segments.length) {
       case 1:
         namespace = DEFAULT_NAMESPACE;
@@ -53,7 +48,23 @@ public class ResourceLocation {
         throw new IllegalArgumentException("Not a valid resource location: \"" + value + "\"");
     }
 
-    return new ResourceLocation(namespace, path);
+    return new ResourceLocation(namespace.intern(), path.intern());
+  }
+
+  /**
+   * @return Whether or not the character is allowed in a resource path.
+   */
+  private static boolean isValidPathChar(char c) {
+    // Paths can contain all the same characters as namespaces, in addition to forward slashes.
+    return isValidNamespaceChar(c) || c == '/';
+  }
+
+  /**
+   * @return Whether or not the character is allowed in a resource namespace.
+   */
+  private static boolean isValidNamespaceChar(char c) {
+    // Namespaces can contain characters a-z (lowercase), 0-9, periods, underscores, and dashes.
+    return (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || c == '.' || c == '_' || c == '-';
   }
 
   @Getter
@@ -71,10 +82,19 @@ public class ResourceLocation {
   }
 
   public ResourceLocation(String namespace, String path) {
-    if (!namespaceFormat.matcher(namespace).matches()) {
-      throw new IllegalArgumentException("Invalid namespace: \"" + namespace + "\"");
-    } else if (!pathFormat.matcher(path).matches()) {
-      throw new IllegalArgumentException("Invalid path: \"" + path + "\"");
+    if (namespace != DEFAULT_NAMESPACE) {
+      for (int i = 0; i < namespace.length(); i++) {
+        if (!isValidNamespaceChar(namespace.charAt(i))) {
+          throw new IllegalArgumentException("Invalid namespace: \"" + namespace + "\"");
+        }
+      }
+    }
+
+
+    for (int i = 0; i < path.length(); i++) {
+      if (!isValidPathChar(path.charAt(i))) {
+        throw new IllegalArgumentException("Invalid path: \"" + path + "\"");
+      }
     }
 
     this.namespace = namespace;
