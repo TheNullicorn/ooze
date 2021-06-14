aa413af85201fdb712b8b1a42fe13f75dcad2f8d
Nullicorn <bennullicorn@gmail.com>
2021-06-14 11:43:17 -0400

    Add chunk buffer interface

---
+++ api/src/main/java/me/nullicorn/ooze/api/storage/ChunkBuffer.java
@@ -0,0 +1,40 @@
+package me.nullicorn.ooze.api.storage;
+
+import java.io.IOException;
+import me.nullicorn.ooze.api.world.BoundedLevel;
+import me.nullicorn.ooze.api.world.Chunk;
+
+/**
+ * Temporary storage for chunks or chunk data. Implementing classes should effectively act as
+ * bridges between NMS chunk storage and Ooze's level-based system.
+ *
+ * @author Nullicorn
+ */
+public interface ChunkBuffer<C extends Chunk> {
+
+    /**
+     * @return The level that provides the buffer with chunks. This is also where {@link #flush()
+     * flushed chunks} are sent.
+     */
+    BoundedLevel<C> getSource();
+
+    /**
+     * Moves any chunks in the buffer into its {@link #getSource() source}, effectively emptying the
+     * buffer.
+     *
+     * @return The number of chunks flushed out into the buffer's source.
+     */
+    int flush() throws IOException;
+
+    /**
+     * Discards any chunks stored in the buffer.
+     *
+     * @return The number of chunks cleared.
+     */
+    int clear();
+
+    /**
+     * @return The number of chunks in the buffer.
+     */
+    int size();
+}
